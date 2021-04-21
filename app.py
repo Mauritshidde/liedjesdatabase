91690c04d5f97201c920f585240ce75ca46c0f29
Mauritshidde <43985321+Mauritshidde@users.noreply.github.com>
2021-04-21 12:16:47 +0200

    remote db
    remote db

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3, json, collections, sys, os
 from forms import ToevoegenForm
-from main import
+#from main import
+liedje = "https://www.youtube.com/watch?v=cPJUBQd-PNM"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd1ccabe4c9d3d5813ba8881bd1082fef'
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+app.run(host='localhost', port=8080, debug=True)
--- forms.py
+++ forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, email_validator, EqualTo
 
 class ToevoegenForm(FlaskForm):
     naam_liedje_toevoegen = StringField('naam van het liedje', validators=[DataRequired()])
-    confirm = SelectField('weet je zekere dat je dit contact wilt torvoegen', choices=["confirm", "cancel"], validators[DataRequired()])
+    confirm = SelectField('weet je zekere dat je dit contact wilt torvoegen', choices=["confirm", "cancel"], validators=[DataRequired()])
     submit = SubmitField('Submit')
